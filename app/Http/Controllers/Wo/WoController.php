238e6894b3aa91b06b62b8628baeee97fb2a75c2
yan <1933679510@qq.com>
2020-10-09 16:02:22 +0800

    登录错误锁定次数

--- app/Http/Controllers/Wo/WoController.php
+++ app/Http/Controllers/Wo/WoController.php
@@ -34,7 +34,6 @@ class WoController extends Controller
     public function logindo(Request $request){
         // $num=Redis::incr('shop_couont');
         // echo $num;exit;
-        // phpinfo();die;
         return view('wo/logindo');
     }
     //执行登录
@@ -44,13 +43,24 @@ class WoController extends Controller
         // except为排除函数
         $user_name=$request->input('user_name');
         $user_password=$request->input('password');
+
+        $key='login:count:'.$user_name;
+        //检查用户已被锁定
+        $count=Redis::get($key);
+        if($count>=5){
+            echo '输入密码错误次数太多，用户已被锁定';die;
+        }
+
         //最后登录的ip
         $ip=$_SERVER['REMOTE_ADDR'];
         $res_login=Womodel::where(['user_name'=>$user_name])
         ->orwhere(['email'=>$user_name])
         ->orwhere(['tel'=>$user_name])
         ->first();
-        if($res_login){
+        if(empty($res_login)){
+            die('用户不存在');
+        }
+        if($res_login['password']==md5($user_password)){
             session('res',['user_name'=>$data['user_name']]);
             $last_login=$res_login['last_login']=time();
             //修改最后登录时间
@@ -64,8 +74,10 @@ class WoController extends Controller
         //    dd($ip);
             return redirect('/wo/aaa');
         }else{
-            // $res=Womodel::where(['password'=>$user_password]);
-            // dd($res);
+            //记录错误次数
+          
+            $count=Redis::incr($key);
+            echo '密码错误次数：'.$count;die;
             return redirect('/wo/logindo')->with(['password'=>'密码错误']);
         }
     }
